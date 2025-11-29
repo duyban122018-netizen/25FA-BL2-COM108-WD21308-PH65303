20c4f4c31d6d5d2c806addb9aa2447a8ee9bb56b
PH653303 <duyban122018@gmail.com>
2025-11-29 16:59:03 +0700

    29/11/25

--- CodeTrenLop/CodeTrenLop/Program.c
+++ CodeTrenLop/CodeTrenLop/Program.c
@@ -2,10 +2,90 @@
 // Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
 
 #include <stdio.h>
+void DEMoMang1Chieu() {
+    // DEMO mang 1 chieu
+    // rarray = a
+    // int array[4];
+    int array[4] = { 100,200,300,400 };
+    // mang duoc danh so tu 0
+    //1
+    scanf("%d", &array[0]);
+    scanf("%d", &array[1]);
+    scanf("%d", &array[2]);
+    scanf("%d", &array[3]);
 
-int main()
-{
-    printf("Hi there!\n");
+    printf("%d", array[0]);printf("\n");
+    printf("%d", array[1]);printf("\n");
+    printf("%d", array[2]);printf("\n");
+    printf("%d", array[3]);printf("\n");
+    //1
+    //1 voi 2 bang nhau deu laf vong lap
+    //2
+    for (int i = 0; i < 4; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%d", array[i]);printf("\n");
+    }
+    //2
+}
+void sapXeptheothutuMang1Chieu() {
+    int integerArray[100];
+    int length;
+    printf("So luong phan tu mang: ");
+    scanf_s("d", &length);
+    printf("Nhap du lieu cho mang %d phan tu\n ", &length);
+    for (int i = 0; i < length; i++)
+    {
+        printf("Mang[%d]= ", i);
+        scanf("%d", &integerArray[i]);
+    }
+    for (int i = 0; i < length; i++)
+    {
+        printf("Mang[%d]= %d\n", i, integerArray[i]);
+
+    }
+}
+
+int main() {
+    int integerArray[100];
+    int i;
+    int tmp; // temperary
+    int length;
+    printf("Nhap so luong phan tu mang: ");
+    scanf("%d", &length);
+    printf("Nhap du lieu mang %d phan tu\n", length);
+    for (i = 0; i < length; i++)
+    {
+        printf("manf[%d] = ", i);
+        scanf("%d", &integerArray[i]);
+    }
+
+    printf("Sap xep tang dan du lieu mang %d phan tu\n", length);
+    for (i = 0; i < length - 1; i++)
+    {
+        if (integerArray[i] > integerArray[i + 1])
+        {
+            tmp = integerArray[i];
+            integerArray[i] = integerArray[i + 1];
+            integerArray[i + 1] = tmp;
+            i = -1;
+        }
+    }
+    printf("Xuat du lieu mang %d phan tu Tang dan\n", length);
+
+    for (i = 0; i < length; i++)
+    {
+        printf("mang[%d] = %d\n", i, integerArray[i]);
+    }
+    printf("Xuat du lieu mang %d phan tu Giam dan\n", length);
+
+    for (i = 0; i < length; i++)
+    {
+        printf("mang[%d] = %d\n", i, integerArray[i]);
+    }
 }
 
 // Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
